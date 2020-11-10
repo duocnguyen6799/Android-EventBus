3bc9fde6e0a65daed6b0bf660e43089628d69781
phucduocnguyen <duoc.nguyen@c2c-techhub.io>
2020-11-10 12:50:08 +0700

    Test branch git

--- app/src/main/java/com/example/eventbusinandroid/MainActivity1.kt
+++ app/src/main/java/com/example/eventbusinandroid/MainActivity1.kt
@@ -1,12 +1,10 @@
 package com.example.eventbusinandroid
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -17,11 +15,13 @@ class MainActivity1 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main1)
         supportActionBar?.title = "ACTIVITY 1"
-        var btnGo:Button = findViewById(R.id.btnGo)
+        var btnGo: Button = findViewById(R.id.btnGo)
         btnGo.setOnClickListener(View.OnClickListener {
-            startActivity(Intent(this,MainActivity2::class.java))
+            startActivity(Intent(this, MainActivity2::class.java))
         })
     }
+
+
     override fun onStart() {
         super.onStart()
         EventBus.getDefault().register(this)
@@ -29,14 +29,14 @@ class MainActivity1 : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        EventBus.getDefault().unregister(this);
+        EventBus.getDefault().unregister(this)
     }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onMessageEvent(msg:MessageEvent){
-        if(msg.msg.equals("FINISH")){
+    fun onMessageEvent(msg: MessageEvent) {
+        if (msg.msg.equals("FINISH")) {
             this.finish()
-            Toast.makeText(applicationContext,"Main Activity 1 has been removed!", Toast.LENGTH_LONG).show()
+            //Toast.makeText(applicationContext, "Main Activity 1 has been removed!", Toast.LENGTH_LONG).show()
         }
     }
     /* for event bus sticky
